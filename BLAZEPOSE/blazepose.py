6470b4d0d60f182cfc79e9dc20cee6d4cfc6d3ab
chungmj <chungmj@dukes.jmu.edu>
2022-04-05 13:50:11 -0400

    Blaze pose update

---
+++ BLAZEPOSE/blazepose.py
@@ -0,0 +1,96 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+cap = cv2.VideoCapture(0)
+
+def make_1080p():
+    cap.set(3, 1920)
+    cap.set(4, 1080)
+
+make_1080p()
+
+# Calculate angle between elbow and body using hip, shoulder, and elbow points
+def calculate_angle(a, b, c):
+    a = np.array(a)  # hip
+    b = np.array(b)  # shoulder
+    c = np.array(c)  # elbow
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+
+# Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        # Make detection
+        results = pose.process(image)
+
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+
+            # Get left body coordinates
+            hip_l = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+            shoulder_l = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                          landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow_l = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                       landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+
+            # Get right body coordinates
+            hip_r = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                     landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            shoulder_r = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                          landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            elbow_r = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                       landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+
+            # Calculate angle
+            angle_l = calculate_angle(hip_l, shoulder_l, elbow_l)
+            angle_r = calculate_angle(hip_r, shoulder_r, elbow_r)
+
+            # Visualize angle
+            cv2.putText(image, str(round(angle_l)),
+                        tuple(np.multiply(shoulder_l, [1920, 1080]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA
+                        )
+
+            cv2.putText(image, str(round(angle_r)),
+                        tuple(np.multiply(shoulder_r, [1920, 1080]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA
+                        )
+        except:
+            pass
+
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                  )
+
+        cv2.imshow('Mediapipe Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
