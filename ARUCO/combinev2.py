fa80b34277a09a9461457bcc67b6d9ec79b16a6b
chungmj <chungmj@dukes.jmu.edu>
2022-11-30 15:08:30 -0500

    thread all functions

--- ARUCO/combinev2.py
+++ ARUCO/combinev2.py
@@ -32,7 +32,7 @@ for onePort in ports:
     portList.append(str(onePort))
     print(str(onePort))
 
-portVar = '/dev/cu.usbserial-1140'
+portVar = '/dev/cu.usbserial-140'
 
 ser.baudrate = 9600
 ser.port = portVar
@@ -59,7 +59,7 @@ param_markers = aruco.DetectorParameters_create()
 
 cap = cv.VideoCapture(1)
 cap2 = cv.VideoCapture(0)
-# cap3 = cv.VideoCapture(2)
+# cap3 = cv.VideoCapture(0)
 
 try:
     fp = open('out.csv', 'w')
@@ -151,116 +151,137 @@ def calculate_grip_width(record_request, grip_messages):
             #     cv.imshow("frame", frame)
 
 
-def calculate_blazepose(pose_record_request):
-    while True:
-        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-            ret1, frame1 = cap2.read()
-            # Recolor image to RGB
-            image = cv.cvtColor(frame1, cv.COLOR_BGR2RGB)
-            image.flags.writeable = False
+def calculate_blazepose(record_request2, pose_messages):
+    recording = False
+    while not shutdown:
+        try:
+            request = record_request2.get(block=False)
+            print(request)
+            recording = request
+        except queue.Empty:
+            # print('no request')
+            if not recording:
+                continue
+        if recording:
+            with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+                ret1, frame1 = cap2.read()
+                # Recolor image to RGB
+                image = cv.cvtColor(frame1, cv.COLOR_BGR2RGB)
+                image.flags.writeable = False
 
-            # Make detection
-            results = pose.process(image)
+                # Make detection
+                results = pose.process(image)
 
-            # Recolor back to BGR
-            image.flags.writeable = True
-            image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+                # Recolor back to BGR
+                image.flags.writeable = True
+                image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
 
-            # Extract landmarks
-            landmarks = results.pose_landmarks.landmark
+                # Extract landmarks
+                landmarks = results.pose_landmarks.landmark
 
-            # Get left body coordinates
-            hip_l = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
-                     landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
-            shoulder_l = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                          landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            elbow_l = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-                       landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                # Get left body coordinates
+                hip_l = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                         landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                shoulder_l = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                              landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                elbow_l = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                           landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
 
-            # Get right body coordinates
-            hip_r = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
-                     landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
-            shoulder_r = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-                          landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-            elbow_r = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
-                       landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                # Get right body coordinates
+                hip_r = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                         landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                shoulder_r = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                              landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                elbow_r = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                           landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
 
-            # Calculate angle
-            angle_l = calculate_angle(hip_l, shoulder_l, elbow_l)
-            angle_r = calculate_angle(hip_r, shoulder_r, elbow_r)
+                # Calculate angle
+                angle_l = calculate_angle(hip_l, shoulder_l, elbow_l)
+                angle_r = calculate_angle(hip_r, shoulder_r, elbow_r)
 
-            # Visualize angle
-            cv.putText(image, str(round(angle_l)),
-                       tuple(np.multiply(shoulder_l, [1280, 720]).astype(int)),
-                       cv.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv.LINE_AA
-                       )
+                # Visualize angle
+                cv.putText(image, str(round(angle_l)),
+                           tuple(np.multiply(shoulder_l, [1280, 720]).astype(int)),
+                           cv.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv.LINE_AA
+                           )
 
-            cv.putText(image, str(round(angle_r)),
-                       tuple(np.multiply(shoulder_r, [1280, 720]).astype(int)),
-                       cv.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv.LINE_AA
-                       )
+                cv.putText(image, str(round(angle_r)),
+                           tuple(np.multiply(shoulder_r, [1280, 720]).astype(int)),
+                           cv.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv.LINE_AA
+                           )
 
-            # Shoulder Distance
-            x = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x - \
-                landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x
-            y = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y - \
-                landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y
-            x_r = x * 40.64 / 0.1
-            y_r = y * 15.24 / 0.1
-            shoulder_distance = math.sqrt(x_r ** 2 + y_r ** 2)
+                # Shoulder Distance
+                x = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x - \
+                    landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x
+                y = landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y - \
+                    landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y
+                x_r = x * 40.64 / 0.1
+                y_r = y * 15.24 / 0.1
+                shoulder_distance = math.sqrt(x_r ** 2 + y_r ** 2)
 
-            print('shoulder distance is: ' + str(shoulder_distance))
+                # print('shoulder distance is: ' + str(shoulder_distance))
 
-            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                                      )
-            # if ret1:
-            #     cv.imshow('Mediapipe Feed', image)
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                          mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                          mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                          )
+                msg_p = (time.time(), angle_l, angle_r, shoulder_distance)
+                pose_messages.put(msg_p, block=False, timeout=None)
 
 
-def calculate_barPath():
-    ret, frame = cap3.read()
+def calculate_barPath(record_request3, bar_messages):
+    recording = False
+    while not shutdown:
+        try:
+            request = record_request3.get(block=False)
+            print(request)
+            recording = request
+        except queue.Empty:
+            if not recording:
+                continue
+        if recording:
+            ret, frame = cap3.read()
 
-    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    marker_corners, marker_IDs, reject = aruco.detectMarkers(
-        gray_frame, marker_dict, parameters=param_markers
-    )
-    if marker_corners:
-        rVec, tVec, _ = aruco.estimatePoseSingleMarkers(
-            marker_corners, MARKER_SIZE_B, cam_mat, dist_coef
-        )
-
-        total_markers = range(0, marker_IDs.size)
-
-        A = np.where(marker_IDs == 25)
-
-        locA = tVec[A]
-
-        fp.flush()
-
-        corners = marker_corners[0].reshape(4, 2)
-        corners = corners.astype(int)
-        top_right = corners[0].ravel()
-        top_left = corners[1].ravel()
-        bottom_right = corners[2].ravel()
-        bottom_left = corners[3].ravel()
-
-        for ids, corners, i in zip(marker_IDs, marker_corners, total_markers):
-            cv.polylines(
-                frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
+            gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            marker_corners, marker_IDs, reject = aruco.detectMarkers(
+                gray_frame, marker_dict, parameters=param_markers
             )
-            corners = corners.reshape(4, 2)
-            corners = corners.astype(int)
-            top_right = corners[0].ravel()
-            top_left = corners[1].ravel()
-            bottom_right = corners[2].ravel()
-            bottom_left = corners[3].ravel()
+            if marker_corners:
+                rVec, tVec, _ = aruco.estimatePoseSingleMarkers(
+                    marker_corners, MARKER_SIZE_B, cam_mat, dist_coef
+                )
 
-            # print(tVec[i])
-            # Draw the pose of the marker
-            point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec[i], tVec[i], 4, 4)
-            print(locA)
+                total_markers = range(0, marker_IDs.size)
+
+                A = np.where(marker_IDs == 25)
+
+                locA = tVec[A]
+
+                fp.flush()
+
+                corners = marker_corners[0].reshape(4, 2)
+                corners = corners.astype(int)
+                top_right = corners[0].ravel()
+                top_left = corners[1].ravel()
+                bottom_right = corners[2].ravel()
+                bottom_left = corners[3].ravel()
+
+                for ids, corners, i in zip(marker_IDs, marker_corners, total_markers):
+                    cv.polylines(
+                        frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
+                    )
+                    corners = corners.reshape(4, 2)
+                    corners = corners.astype(int)
+                    top_right = corners[0].ravel()
+                    top_left = corners[1].ravel()
+                    bottom_right = corners[2].ravel()
+                    bottom_left = corners[3].ravel()
+
+                    # print(tVec[i])
+                    # Draw the pose of the marker
+                    point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec[i], tVec[i], 4, 4)
+                    msg_bar = (time.time(), locA)  # Message is a tuple of (current time, grip width)
+                    grip_messages.put(msg_bar, block=False, timeout=None)
 
 
 def calculate_arduino(arduino_messages):
@@ -292,10 +313,12 @@ def calculate_arduino(arduino_messages):
 
 
 grip_record_request = queue.Queue()
-# pose_record_request = queue.Queue()
-# bar_record_request = queue.Queue()
+pose_record_request = queue.Queue()
+bar_record_request = queue.Queue()
 
 grip_messages = queue.Queue()
+pose_messages = queue.Queue()
+bar_messages = queue.Queue()
 arduino_messages = queue.Queue()
 rack_state = queue.Queue()
 accel_messages = queue.Queue()
@@ -306,10 +329,10 @@ t0.start()
 t1 = threading.Thread(target=calculate_grip_width, args=(grip_record_request, grip_messages))
 t1.start()
 
-# t2 = threading.Thread(target=calculate_blazepose, args=(pose_record_request,))
-# t2.start()
+t2 = threading.Thread(target=calculate_blazepose, args=(pose_record_request, pose_messages))
+t2.start()
 
-# t3 = threading.Thread(target=calculate_barPath(), args=())
+# t3 = threading.Thread(target=calculate_barPath, args=(bar_record_request, bar_messages))
 # t3.start()
 
 while True:
@@ -340,18 +363,35 @@ while True:
         racked = rack_state.get(block=False)
         print(str(racked) + ' on the main thread')
         grip_record_request.put(racked)
+        # bar_record_request.put(racked)
+        pose_record_request.put(racked)
 
     except queue.Empty:
-        # print('no rack state')
         pass
 
     try:
-        message = grip_messages.get(block=False)
-        print(message[1])
+        grip_data = grip_messages.get(block=False)
+        print('grip width: ' + str(grip_data[1]))
+
     except queue.Empty:
         pass
 
+    try:
+        pose_data = pose_messages.get(block=False)
+        print('left elbow angle ' + str(pose_data[1]))
+        print('right elbow angle ' + str(pose_data[2]))
+        print('shoulder distance: ' + str(pose_data[3]))
+
+    except queue.Empty:
+        pass
+
+    # try:
+    #     bar_data = bar_messages.get(block=False)
+    #     print(bar_data[1])
+    # except queue.Empty:
+    #     pass
+
 t0.join()
 t1.join()
-# t2.join()
+t2.join()
 # t3.join()
